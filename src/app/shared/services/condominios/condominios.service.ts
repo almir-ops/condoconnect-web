008d4f3310a127d71485d03c89e8c32e92511c2d
Almir Libório Batista Junior <almir_jesus2@hotmail.com>
2025-03-14 16:02:07 -0300

    att

--- src/app/shared/services/condominios/condominios.service.ts
+++ src/app/shared/services/condominios/condominios.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../../../environments/environment';
   providedIn: 'root'
 })
 export class CondominiosService {
-  private readonly apiUrl = environment.apiUrl + 'establishments';
+  private readonly apiUrl = environment.apiUrl + 'condominios';
 
   constructor(private http: HttpClient) {}
 
