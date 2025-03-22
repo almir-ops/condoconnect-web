58bd297838c43a5a3e19e8aa50d850a67d0f253b
unknown <almir-ops>
2025-03-22 19:29:29 -0300

    att

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,14 @@ export const routes: Routes = [
     component:HomeComponent
   },
   {
+    path: 'app',
+    component:HomeComponent
+  },
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
     path: '',
     redirectTo: '',
     pathMatch: 'full'
--- src/app/shared/components/dialog/alert.service.ts
+++ src/app/shared/components/dialog/alert.service.ts
@@ -8,16 +8,24 @@ import { AlertComponent } from '../alert/alert.component';
 export class AlertService {
   constructor(private dialog: MatDialog) {}
 
-  presentAlert(header: string, message: string, okCallback?: () => void) {
+  presentAlert(header: string, message: string, okCallback?: () => void, duration: number = 5000) {
     const dialogRef = this.dialog.open(AlertComponent, {
       width: '350px',
       data: { header, message },
     });
 
+    // Fechar o alerta automaticamente após o tempo definido
+    setTimeout(() => {
+      dialogRef.close();
+    }, duration);
+
     dialogRef.afterClosed().subscribe(() => {
+      console.log('das');
+
       if (okCallback) {
         okCallback(); // Executa o callback após fechar
       }
     });
   }
+
 }
